8beb6e51ecdd05ddf5de555e160451d90ed940d5
edsonmanuelcarballovera <edsonmanuelcarballovera@gmail.com>
2025-02-06 12:01:46 -0600

    fixing code style

--- src/main/java/com/sintergica/apiv2/controlador/Admin.java
+++ src/main/java/com/sintergica/apiv2/controlador/Admin.java
@@ -97,8 +97,6 @@ public class Admin {
         .body("El usuario ya pertenece a una compañía");
   }
 
-
-
   @PreAuthorize("hasRole('ADMIN')")
   @GetMapping("/listCompany")
   public ResponseEntity<List<Map<String, Object>>> listCompany() {
