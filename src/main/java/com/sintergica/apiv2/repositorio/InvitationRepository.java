bfe19aeafa007012a42abe0e803ca86ee3de1ac1
JuanPablo-270 <juan.peredo@sintergica.ai>
2025-02-13 13:16:31 -0600

    fix: Code Styles

--- src/main/java/com/sintergica/apiv2/repositorio/InvitationRepository.java
+++ src/main/java/com/sintergica/apiv2/repositorio/InvitationRepository.java
@@ -1,11 +1,10 @@
 package com.sintergica.apiv2.repositorio;
 
 import com.sintergica.apiv2.entidades.Invitation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Date;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InvitationRepository extends JpaRepository<Invitation, UUID> {
-    Invitation findByExpireDate(Date expireDate);
+  Invitation findByExpireDate(Date expireDate);
 }
